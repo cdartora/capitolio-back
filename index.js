bfbc844f328f589b8d3677810310ab535e098001
Carlos Dartora <carlosdalsoler@gmail.com>
2023-10-31 16:26:36 -0300

    feat: setup logger (winston)

--- index.js
+++ index.js
@@ -2,43 +2,65 @@ const puppeteer = require("puppeteer");
 const { createEvent, createEventData } = require("./calendar");
 const { scrapeMovies, scrapeDate } = require("./scraping");
 
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
 console.log("Running the script on a 7-day schedule on Fridays.");
 
 const main = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const [page] = await browser.pages();
-  await page.setViewport({ width: 1200, height: 720 });
-  const url = "http://www.capitolio.org.br/programacao/";
-  await page.goto(url, { waitUntil: "domcontentloaded" });
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const [page] = await browser.pages();
+    await page.setViewport({ width: 1200, height: 720 });
+  } catch (err) {
+    logger.error("Something setting up puppetteer failed:", err);
+  }
+
+  try {
+    const url = "http://www.capitolio.org.br/programacao/";
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+  } catch (err) {
+    logger.error("Couldn't reach Capitólio website:", err);
+  }
 
   const dateHrefs = await page.$$eval(".dates a.date", (elements) =>
     elements.map((element) => element.href)
   );
-  console.log(dateHrefs);
 
   for (const url of dateHrefs) {
     await page.goto(url);
 
-    const movieData = await scrapeMovies(page);
-    const movieDate = await scrapeDate(page);
+    const movieData = await scrapeMovies(page).catch((err) =>
+      logger.error("movie scraping failed:", err)
+    );
+
+    const movieDate = await scrapeDate(page).catch((err) =>
+      logger.error("movie scraping failed:", err)
+    );
 
     for (const movie of movieData) {
       const eventData = createEventData({ ...movie, movieDate });
-      try {
-        const link = await createEvent(eventData);
-        console.log("Event added to calendar:", link);
-        // console.log(eventData.summary, eventData.start.dateTime);
-      } catch (error) {
-        console.error(error);
-      }
+      await createEvent(eventData).catch((err) =>
+        logger.error("Add movie event to calendar failed:", err)
+      );
     }
   }
 
+  logger.log(`Agenda for day ${movieDate} complete!`);
   browser.close();
 };
 
 try {
   main();
+  logger.log("Agenda for the week completed successfully!!");
 } catch (err) {
-  console.log(err);
+  logger.error("Error:", err);
 }
